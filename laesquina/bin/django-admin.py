ff104791540497822def4b3e79a2121a9aee06cd
Emilio Madrid <emadridabarca@gmail.com>
2024-04-10 13:25:41 -0400

    Primer commit

---
+++ laesquina/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/emiliomadrid/BootcampPython/Modulo 6/Desafio - 5 entornos/laesquina/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
